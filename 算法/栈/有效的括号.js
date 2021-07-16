eded00b06deb0db11ccac05ca2db64e51a60e0f2
lizhuo <lizhuo@pwrd.com>
2021-07-16 10:13:17 +0800

    day1

--- 算法/栈/有效的括号.js
+++ 算法/栈/有效的括号.js
@@ -1,19 +1,26 @@
 // 时间复杂度O(n)
-const isValid = function(s){
-    let stack = [];
-    s.split('').forEach((item) => {
-        let stack = [];
-        console.log(stack)
-        // if (item == '(') {
-        //     stack.push(')')
-        // } else if (item == '[') {
-        //     stack.push(']')
-        // } else if (item == '{') {
-        //     stack.push('}')
-        // } else if (stack.pop() !== item) {
-        //     return false;
-        // }
-        // return !stack.length
-    })
+const isValid = (s)=>{
+    const stack = [];
+    const res = s.split('').every((item) => {
+        if (item == '(') {
+            stack.push(')')
+        } else if (item == '[') {
+            stack.push(']')
+        } else if (item == '{') {
+            stack.push('}')
+        } else if (stack.pop() !== item) {
+            return false;
+        }
+        return true;
+     });
+     return !stack.length&&res
 }
-isValid('()')
+// var a = [1, 2, 3];
+// var res = a.every((item) => {
+//     console.log(item)
+//     if (item !== 1) {
+//         console.log('进来了')
+//         return false;
+//     }
+// });
+// console.log(res)
