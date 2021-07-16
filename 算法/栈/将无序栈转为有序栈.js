a8f0a0bea616b8a84927af8c8b8fa67390191eae
lizhuo <lizhuo@pwrd.com>
2021-07-16 18:42:27 +0800

    day2

---
+++ 算法/栈/将无序栈转为有序栈.js
@@ -0,0 +1,23 @@
+// 时间复杂度O(n2) 空间复杂度O(n)
+const sort = stack => {
+    const _sort = () => {
+     if(stack.length<=1) return stack;
+     let num=stack.pop();
+     if(num<stack[stack.length-1]){
+      let temp=stack.pop();
+      stack.push(num);
+      num=temp;
+      _sort();
+     }else{
+      _sort();
+     }
+     stack.push(num);
+    }
+    let index=stack.length;
+    while(index>0){
+     _sort();   //每一次排序完栈底元素最小 
+     index--;
+    }
+    return stack;
+}
+console.log(sort([1, 5, 4, 3, 2, 4, 8]));
